0703bf4525030675cd60887e28e5f3252ba6c5d2
867380699 <867380699@qq.com>
2018-02-26 16:05:13 +0800

    修复了不能正确处理空格的BUG

--- new_post.sh
+++ new_post.sh
@@ -1,10 +1,11 @@
 #!/usr/bash
 date=`date +%Y-%m-%d`
-post=./_posts/"$date-$1.md"
-touch $post
+post="./_posts/$date-$1.md"
+touch "$post"
 echo "---\n
 layout: post
 title: \"$1\"
 date: $date\n
 ---">$post
 echo "$1"
+echo "$post"
