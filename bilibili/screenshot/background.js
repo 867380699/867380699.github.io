5d2169d7753e344ef8e4a91fa152f8016c586118
867380699 <867380699@qq.com>
2020-01-13 13:35:34 +0800

    use downloads API

--- bilibili/screenshot/background.js
+++ bilibili/screenshot/background.js
@@ -4,27 +4,54 @@ chrome.runtime.onInstalled.addListener(function() {
       conditions: [new chrome.declarativeContent.PageStateMatcher({
         pageUrl: {hostEquals: 'www.bilibili.com'},
       })],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
+      actions: [
+        new chrome.declarativeContent.ShowPageAction(),
+      ]
     }]);
   });
 });
+
 chrome.runtime.onMessage.addListener(
 function(message, callback) {
   console.log('message', message);
 });
+
 chrome.commands.onCommand.addListener(function(command) {
-  console.log('Command:', command);
   switch(command) {
     case "screenshot":
-      console.log('take screenshot.');
+      console.log('Command: screenshot.');
 
       chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        console.log('script')
         chrome.tabs.executeScript(
-            tabs[0].id,
-            {file: 'contentScript.js'});
+          tabs[0].id,
+          {file: 'contentScript.js'},
+          screenshotCallback,
+        );
       });
       break;
   }
 });
 
+var screenshotCallback = function(result) {
+  if (result && typeof result[0] === 'string' && result[0].startsWith('data:image')) {
+    var filename = getFileName();
+    chrome.downloads.download({url:result[0], filename: filename}, function() {
+
+    });
+  }
+}
+
+function getFileName(){
+  var date = new Date();
+  return [
+    date.getFullYear(),
+    date.getDate().toString().padStart(2,0),
+    date.getDay().toString().padStart(2,0),
+    date.getHours().toString().padStart(2,0),
+    date.getMinutes().toString().padStart(2,0),
+    date.getSeconds().toString().padStart(2,0),
+    date.getMilliseconds().toString().padStart(3,0),
+    (Math.random()*1000).toFixed(0).padStart(3,0),
+    '.png'
+  ].join('')
+}
--- bilibili/screenshot/contentScript.js
+++ bilibili/screenshot/contentScript.js
@@ -1,17 +1,3 @@
-function getFileName(){
-  var date = new Date();
-  return [
-    date.getFullYear(),
-    date.getDate().toString().padStart(2,0),
-    date.getDay().toString().padStart(2,0),
-    date.getHours().toString().padStart(2,0),
-    date.getMinutes().toString().padStart(2,0),
-    date.getSeconds().toString().padStart(2,0),
-    date.getMilliseconds().toString().padStart(3,0),
-    (Math.random()*1000).toFixed(0).padStart(3,0)
-  ].join('')
-}
-
 function screenshot() {
   var video = document.querySelector('video')
   var canvas = document.createElement('canvas');
@@ -20,9 +6,6 @@ function screenshot() {
   var ctx = canvas.getContext('2d');
   ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
   var dataURI = canvas.toDataURL();
-  var link = document.createElement("a");
-  link.download = getFileName();
-  link.href = dataURI;
-  link.click();
+  return dataURI;
 }
 screenshot();
