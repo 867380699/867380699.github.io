79d39c6a4c06e6f6237560e592e21fd09acf5b21
867380699 <867380699@qq.com>
2020-10-22 19:55:00 +0800

    feat: add BundleAnalyzerPlugin & splitChunksPlugin

--- chrome-ext/squirrelHouse/squirrelHouse/webpack.config.js
+++ chrome-ext/squirrelHouse/squirrelHouse/webpack.config.js
@@ -1,13 +1,24 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyPlugin = require('copy-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   entry: {
     popup: ['./src/popup.js', './src/styles/popup.scss'],
     background: './src/background.js',
   },
+  optimization: {
+    splitChunks: {
+      name: 'base',
+      chunks: 'all',
+      minSize: 15000,
+    }
+  },
   plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerPort: 8899
+    }),
     new MiniCssExtractPlugin(),
     new CopyPlugin([
       {
