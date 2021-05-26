a1bb56b503d44ca82c67580de71b8f1f1c32982f
867380699 <867380699@qq.com>
2021-05-26 14:02:31 +0800

    disable cache

--- sw.js
+++ sw.js
@@ -1,15 +1,15 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('io-store').then((cache) => cache.addAll([
-      '/blog/',
-      '/search/index.html',
-    ])),
-  );
-});
+// self.addEventListener('install', (e) => {
+//   e.waitUntil(
+//     caches.open('io-store').then((cache) => cache.addAll([
+//       '/blog/',
+//       '/search/index.html',
+//     ])),
+//   );
+// });
 
-self.addEventListener('fetch', (e) => {
-  console.log(e.request.url);
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request)),
-  );
-});
+// self.addEventListener('fetch', (e) => {
+//   console.log(e.request.url);
+//   e.respondWith(
+//     caches.match(e.request).then((response) => response || fetch(e.request)),
+//   );
+// });
