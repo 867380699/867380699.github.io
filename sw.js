7d959e424e295614356a497ca54e7f26464b97fa
867380699 <867380699@qq.com>
2021-05-27 11:19:07 +0800

    feat: new cache machanism

--- sw.js
+++ sw.js
@@ -1,15 +1,35 @@
-// self.addEventListener('install', (e) => {
-//   e.waitUntil(
-//     caches.open('io-store').then((cache) => cache.addAll([
-//       '/blog/',
-//       '/search/index.html',
-//     ])),
-//   );
-// });
+const storeName = 'io-store-v3';
+const cacheRegexp = new RegExp(`${location.host}/(js|css|assets)/.+`);
 
-// self.addEventListener('fetch', (e) => {
-//   console.log(e.request.url);
-//   e.respondWith(
-//     caches.match(e.request).then((response) => response || fetch(e.request)),
-//   );
-// });
+const fetchWithCacahe = async (request) => {
+  const matchedResponse = await caches.match(request);
+  if (matchedResponse) {
+    return matchedResponse;
+  } else {
+    const store = await caches.open(storeName);
+    const response = await fetch(request);
+    if (response.ok) {
+      if (cacheRegexp.test(request.url)) {
+        await store.put(request.url, response.clone());
+      }
+    }
+    return response;
+  }
+}
+
+self.addEventListener('fetch', (e) => {
+  console.log(e.request.url);
+  e.respondWith(fetchWithCacahe(e.request));
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key != storeName) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
